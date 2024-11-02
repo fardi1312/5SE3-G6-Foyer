e97e9429cb0d052840ceed41ff6a4e80b52dc29b
MASSOUD <Massoud.fradi@esprit.tn>
2024-11-02 09:35:09 +0100

    Update .gitignore to exclude test files

--- src/test/java/tn/esprit/spring/RestControllersTest/ChambreRestControllerTest.java
+++
@@ -1,4 +0,0 @@
-package tn.esprit.spring.RestControllersTest;
-
-public class ChambreRestControllerTest {
-}
--- src/test/java/tn/esprit/spring/ServicesTest/Chambre/ChambreServiceTest.java
+++
@@ -1,6 +0,0 @@
-package tn.esprit.spring.ServicesTest.Chambre;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class ChambreServiceTest {
-}
