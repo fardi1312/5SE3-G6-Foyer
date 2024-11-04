57fc2e2f0d5edd2c2c68d0a87f54c01be169d7d0
MASSOUD <Massoud.fradi@esprit.tn>
2024-11-04 12:13:14 +0100

    commit test

--- src/test/java/tn/esprit/spring/FoyerApplicationTests.java
+++ src/test/java/tn/esprit/spring/FoyerApplicationTests.java
@@ -1,11 +1,12 @@
 package tn.esprit.spring;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-public class FoyerApplicationTests {
-
-
-
-
+public  class FoyerApplicationTests {
+    @Test
+    void contextLoads() {
+        System.out.println("methode test");
+    }
 }
